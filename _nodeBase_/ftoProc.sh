f80e5210c7a2166274f618d5a4370602beafb0c5
Mohsen Banan <git@mohsen.1.banan.byname.net>
2020-12-26 22:34:02 -0800

    More dblock updates.

--- _nodeBase_/ftoProc.sh
+++ _nodeBase_/ftoProc.sh
@@ -16,7 +16,7 @@ SEED="
 *  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedFtoCommon.sh]] | 
 "
 FILE="
-*  /This File/ :: /bisos/panels/bxde/model/_nodeBase_/ftoProc.sh 
+*  /This File/ :: /bisos/panels/bxde-model/_nodeBase_/ftoProc.sh 
 "
 if [ "${loadFiles}" == "" ] ; then
     /bisos/core/bsip/bin/seedFtoCommon.sh -l $0 "$@" 
--- ftoProc.sh
+++ ftoProc.sh
@@ -16,7 +16,7 @@ SEED="
 *  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedFtoCommon.sh]] | 
 "
 FILE="
-*  /This File/ :: /bisos/panels/bxde/model/ftoProc.sh 
+*  /This File/ :: /bisos/panels/bxde-model/ftoProc.sh 
 "
 if [ "${loadFiles}" == "" ] ; then
     /bisos/core/bsip/bin/seedFtoCommon.sh -l $0 "$@" 
